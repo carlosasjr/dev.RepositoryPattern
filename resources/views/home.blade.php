ec5d0bfb0276b9133619ffb5ec8fd876eedac6a8
Carlos Junior <carlos@theplace.com.br>
2019-11-01 14:56:46 -0300

    Primeiro Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'SoftPro - Advogados')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Você está logado!</p>
+@stop
